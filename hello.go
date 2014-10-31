29d9486d26d98eb1ead0fc2e0dda0c7731f4c4f4
Krzysztof Kula <krzychukula@gmail.com>
2014-10-31 07:40:19 +0100

    use new Sort and diff it with math
    https://tour.golang.org/#25

--- hello.go
+++ hello.go
@@ -3,8 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/krzychukula/newmath"
+	"math"
 )
 
 func main() {
-	fmt.Printf("Hello, world. Sqrt(2) = %v\n", newmath.Sqrt(2))
+	fmt.Printf("Sqrt(2) = %v. math.Sqrt(2) = %v. Diff:%v\n", newmath.Sqrt(2), math.Sqrt(2), newmath.Sqrt(2)-math.Sqrt(2))
 }
